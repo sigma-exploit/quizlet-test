dc22c78117510e0fa28331b2ec21308f3a486054
sigma-exploit <goofy_01234@outlook.com>
2025-03-10 18:55:45 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+function generateQuiz () {
+  var quizContents = document.getElementById("quizcontents").value;
+  if (quizContents == "") {
+    alert("Please enter valid quiz contents");
+    console.log("Generation stopped because no quiz contents were entered");
+  }
+  var quizContents_line = quizContents.split("\n");
+  var quizContents_final = [];
+  for (var i = 0; i < quizContents_line.length; i++) {
+    quizContents_final.push(quizContents_line.split(": "));
+  }
+  window.open(); // TODO: replace with final link later
+}
